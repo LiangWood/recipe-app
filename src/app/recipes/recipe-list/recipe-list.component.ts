b32429efdd6767e6d645febb41f5ea8e9af7561f
Jack Liang <jack.liang.tw@pmgi.local>
2019-06-12 17:15:32 +0800

    add header/recipe/shoppinglist DOM

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://img.taste.com.au/vdX2cNgW/w733-h489-cfill-q80/taste/2019/01/mini-leek-fittatas-146677-1.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDxz7JB3Jn1vepIhH0xufnLlcOqztGDKM0z3_FsKmx6SZesvcQ'),
+  ];
 
   constructor() { }
 
