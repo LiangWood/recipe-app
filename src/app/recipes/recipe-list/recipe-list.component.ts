82f4fa73806a767d54536c5b0e56f40d9c20ef78
梁文賓 <jack@liangwenbinde-MacBook-Pro.local>
2019-06-15 22:48:25 +0800

    add shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,22 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    // tslint:disable-next-line:max-line-length
+    // tslint:disable-next-line: max-line-length
     new Recipe('A Test Recipe', 'This is simply a test', 'https://img.taste.com.au/vdX2cNgW/w733-h489-cfill-q80/taste/2019/01/mini-leek-fittatas-146677-1.jpg'),
+    // tslint:disable-next-line: max-line-length
     new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDxz7JB3Jn1vepIhH0xufnLlcOqztGDKM0z3_FsKmx6SZesvcQ'),
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
