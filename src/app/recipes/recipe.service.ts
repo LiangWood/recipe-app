d85f4ea7d5356ed0967b5e48c540d441f3afbbff
梁文賓 <jack@liangwenbinde-MacBook-Pro.local>
2019-06-23 23:13:35 +0800

    Service

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,42 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { Recipe } from './recipe.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+
+  constructor(private slService: ShoppingListService) {}
+
+  recipeSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+    // tslint:disable-next-line: max-line-length
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://img.taste.com.au/vdX2cNgW/w733-h489-cfill-q80/taste/2019/01/mini-leek-fittatas-146677-1.jpg',
+      [
+        new Ingredient('test', 1),
+        new Ingredient('test', 2)
+      ]),
+    // tslint:disable-next-line: max-line-length
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDxz7JB3Jn1vepIhH0xufnLlcOqztGDKM0z3_FsKmx6SZesvcQ',
+      [
+        new Ingredient('test', 10),
+        new Ingredient('test', 20)
+      ]),
+  ];
+
+  getRecipe() {
+    return this.recipes.slice();
+  }
+
+  addRecipeToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+}
